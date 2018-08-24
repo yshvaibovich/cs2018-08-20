4c1d7057a1f6561afe5454e8dc64d8be413abc65
nadezhda <nadezhdaboltach@gmail.com>
2018-08-24 12:32:37 +0300

    Merge branch 'master' of https://github.com/yshvaibovich/cs2018-08-20
    # Conflicts:
    #	src/by/it/_tasks_/lesson02/TaskA2.java

--- src/by/it/shvaibovich/lesson03/TaskA2.java
+++ src/by/it/shvaibovich/lesson03/TaskA2.java
@@ -22,11 +22,11 @@ Lesson 03. Task A2. Стихотворение
 */
 class TaskA2 {
     public static void main(String[] args) {
-        String a ="asdad";
-                String b="qweeqw";
-        String c="qeqweqttas";
-                String d="xcvxcvxcv";
-        String e="sxcbdsgb";
+        String a ="asdgfhdfghdfghdgfhdfghdfad";
+                String b="qweefghddfhgdfghdfghdfghqw";
+        String c="qeqweqttdfghdfghdfghdfhdfhas";
+                String d="xcvxcdfhdfhdfhdfhdfhdfhvxcv";
+        String e="sxcbdhdfghgfdhfdsgb";
         System.out.println("Мое любимое стихотворение:");
                System.out.println(a+"\n"+b+"\n"+c+"\n"+d+"\n"+e);
     }
