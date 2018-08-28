dcbce5f4c48422a044eb5de6cff7675038a6c3c7
yshvaibovich <y.shvaibovich@gmail.com>
2018-08-28 21:27:15 +0300

    111

--- src/by/it/shvaibovich/lesson04/TaskB3.java
+++ src/by/it/shvaibovich/lesson04/TaskB3.java
@@ -31,10 +31,11 @@ public class TaskB3 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int x=sc.nextInt();
-        int sum=1;
-        for(int i=1;sum<x;i++){
+        int sum=0;
+        for(int i=1;sum<=x;i++){
+                        sum=sum+i;
+                        if (sum<=x)
             System.out.print(sum+",");
-            sum=sum+i;
         }
     }
 }
